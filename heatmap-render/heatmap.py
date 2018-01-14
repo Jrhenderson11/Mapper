4e02c2d3adaafc4cec86ebdb628702eb25d2b3ed
James <jrhenderson11@gmail.com>
2018-01-14 16:25:41 +0000

    added command line option to heatmap + made clolour absolute

--- heatmap-render/heatmap.py
+++ heatmap-render/heatmap.py
@@ -1,3 +1,4 @@
+import sys
 import math
 import pygame
 
@@ -57,25 +58,36 @@ def readmap(fname):
 			grid[y].append(int(lines[y].split(",")[x]))
 	return grid
 
+def get_max(grid):
+	maximum =0
+	for y in range(len(grid)):
+		for x in range(len(grid[0])):
+			if grid[y][x] > maximum:
+				maximum = grid[y][x]
+	return maximum
 
-def drawmap(grid):
+def drawmap(grid, name):
 	window_height = 1000
 	window_width = 1000
 	screen = pygame.display.set_mode((window_width, window_height))
-	screen.fill(WHITE)
+	#screen.fill(WHITE)
 
-	#pygame.display.set_caption("Heatmap: " + fname)
+	pygame.display.set_caption("Heatmap: " + fname)
 	height = len(grid)
 	width = len(grid[0])
 	cell_width = window_width / width
 	cell_height = window_height / height
 	grad = gradient_func([(0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 0)])
+
+	 
+	#maximum = get_max(grid)
+	maximum = 255
 	for y in range(height):
 		for x in range(width):
 			#make rect
 			rectangle = ((x*cell_width), (y*cell_height), cell_width, cell_height)  
 			#print rectangle
-			colour = grad(float(grid[y][x]) / 5.0)
+			colour = grad(float(grid[y][x]) / maximum)
 
 			pygame.draw.rect(screen, colour, rectangle)
 	pygame.display.flip()
@@ -83,9 +95,13 @@ def drawmap(grid):
 	
 
 
-grid = readmap("map.txt")
-print grid
-drawmap(grid)
+fname = "../JMapper/files/map.txt"
+#fname = "map.txt"
+if len(sys.argv) == 2:
+	fname = sys.argv[1]
+
+grid = readmap(fname)
+drawmap(grid, fname)
 done = False
 while (not done):
 	for event in pygame.event.get():  # User did something
