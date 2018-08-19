4a065dc96373f0b8d45ea53287ecd5b21155dec0
James <jrhenderson11@gmail.com>
2018-08-19 17:46:45 +0100

    added Shallow_sea biome

--- JMapper/src/version2/Terrain.java
+++ JMapper/src/version2/Terrain.java
@@ -11,7 +11,7 @@ import version2.FastNoise.NoiseType;
 public class Terrain {
 
 	public enum Biome {
-		SEA, WATER, DESERT, TEMPERATE_DESERT, SUBTROPICAL_DESERT, SHRUBLAND, GRASSLAND, SAVANNAH, TAIGA, FOREST, TEMPERATE_DECIDUOUS_FOREST, TEMPERATE_RAIN_FOREST, TROPICAL_RAIN_FOREST, TROPICAL_SEASONAL_FOREST, BEACH, SCORCHED, BARE, TUNDRA, SNOW
+		SEA, SHALLOW_SEA, WATER, DESERT, TEMPERATE_DESERT, SUBTROPICAL_DESERT, SHRUBLAND, GRASSLAND, SAVANNAH, TAIGA, FOREST, TEMPERATE_DECIDUOUS_FOREST, TEMPERATE_RAIN_FOREST, TROPICAL_RAIN_FOREST, TROPICAL_SEASONAL_FOREST, BEACH, SCORCHED, BARE, TUNDRA, SNOW
 	}
 
 	private int size = 300;
@@ -28,63 +28,19 @@ public class Terrain {
 		this.biome = triple.getBiome();
 	}
 
-	public List<Point.Double> getTreePositions() {
-		HashMap<Biome, Integer> TREE_PROBABILITY_TABLE = new HashMap<Biome, Integer>();
-		List<Point.Double> trees = new ArrayList<>();
-		TREE_PROBABILITY_TABLE.put(Biome.SEA, 100);
-		TREE_PROBABILITY_TABLE.put(Biome.WATER, 100);
-		TREE_PROBABILITY_TABLE.put(Biome.DESERT, 9);
-		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_DESERT, 9);
-		TREE_PROBABILITY_TABLE.put(Biome.SUBTROPICAL_DESERT, 8);
-		TREE_PROBABILITY_TABLE.put(Biome.SHRUBLAND, 6);
-		TREE_PROBABILITY_TABLE.put(Biome.GRASSLAND, 7);
-		TREE_PROBABILITY_TABLE.put(Biome.SAVANNAH, 6);
-		TREE_PROBABILITY_TABLE.put(Biome.TAIGA, 4);
-		TREE_PROBABILITY_TABLE.put(Biome.FOREST, 2);
-		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_DECIDUOUS_FOREST, 2);
-		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_RAIN_FOREST, 1);
-		TREE_PROBABILITY_TABLE.put(Biome.TROPICAL_RAIN_FOREST, 1);
-		TREE_PROBABILITY_TABLE.put(Biome.TROPICAL_SEASONAL_FOREST, 1);
-		TREE_PROBABILITY_TABLE.put(Biome.BEACH, 100);
-		TREE_PROBABILITY_TABLE.put(Biome.SCORCHED, 9);
-		TREE_PROBABILITY_TABLE.put(Biome.BARE, 100);
-		TREE_PROBABILITY_TABLE.put(Biome.TUNDRA, 15);
-		TREE_PROBABILITY_TABLE.put(Biome.SNOW, 100);
-		// int R = 3;
-		// from https://www.redblobgames.com/maps/terrain-from-noise/#trees
-
-		FastNoise generator = new FastNoise(new Random().nextInt());
-		generator.SetNoiseType(NoiseType.Value);
-		for (int yc = 0; yc < size; yc++) {
-			for (int xc = 0; xc < size; xc++) {
-				double max = 0;
-				// there are more efficient algorithms than this
-				int R = TREE_PROBABILITY_TABLE.get(biome[xc][yc]);
-				for (int yn = yc - R; yn <= yc + R; yn++) {
-					for (int xn = xc - R; xn <= xc + R; xn++) {
-						double e = generator.GetWhiteNoiseInt(xn, yn);
-						if (e > max) {
-							max = e;
-						}
-					}
-				}
-				if (generator.GetWhiteNoiseInt(xc, yc) == max) {
-					// place tree at xc,yc
-					trees.add(new Point.Double(xc, yc));
-				}
-			}
-		}
-		return trees;
-	}
-
 	public Terrain(int newSize) {
 		this.size = newSize;
 	}
 
 	public static Biome getBiome(double e, double m, double sealevel) {
 
-		if (e < sealevel)
-			return Biome.SEA;
+		if (e < sealevel) {
+			if (e > sealevel - 0.015) {
+				return Biome.SHALLOW_SEA;
+			} else {
+				return Biome.SEA;
+			}
+		}
 		if (e < sealevel + 0.01)
 			return Biome.BEACH;
 
@@ -163,6 +119,56 @@ public class Terrain {
 
 	public void setSealevel(double sealevel) {
 		this.sealevel = sealevel;
+	}
+
+	public List<Point.Double> getTreePositions() {
+		HashMap<Biome, Integer> TREE_PROBABILITY_TABLE = new HashMap<Biome, Integer>();
+		List<Point.Double> trees = new ArrayList<>();
+		TREE_PROBABILITY_TABLE.put(Biome.SEA, 100);
+		TREE_PROBABILITY_TABLE.put(Biome.SHALLOW_SEA, 100);
+		TREE_PROBABILITY_TABLE.put(Biome.WATER, 100);
+		TREE_PROBABILITY_TABLE.put(Biome.DESERT, 9);
+		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_DESERT, 9);
+		TREE_PROBABILITY_TABLE.put(Biome.SUBTROPICAL_DESERT, 8);
+		TREE_PROBABILITY_TABLE.put(Biome.SHRUBLAND, 6);
+		TREE_PROBABILITY_TABLE.put(Biome.GRASSLAND, 7);
+		TREE_PROBABILITY_TABLE.put(Biome.SAVANNAH, 6);
+		TREE_PROBABILITY_TABLE.put(Biome.TAIGA, 4);
+		TREE_PROBABILITY_TABLE.put(Biome.FOREST, 3);
+		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_DECIDUOUS_FOREST, 3);
+		TREE_PROBABILITY_TABLE.put(Biome.TEMPERATE_RAIN_FOREST, 1);
+		TREE_PROBABILITY_TABLE.put(Biome.TROPICAL_RAIN_FOREST, 1);
+		TREE_PROBABILITY_TABLE.put(Biome.TROPICAL_SEASONAL_FOREST, 1);
+		TREE_PROBABILITY_TABLE.put(Biome.BEACH, 100);
+		TREE_PROBABILITY_TABLE.put(Biome.SCORCHED, 9);
+		TREE_PROBABILITY_TABLE.put(Biome.BARE, 100);
+		TREE_PROBABILITY_TABLE.put(Biome.TUNDRA, 15);
+		TREE_PROBABILITY_TABLE.put(Biome.SNOW, 100);
+		// int R = 3;
+		// from https://www.redblobgames.com/maps/terrain-from-noise/#trees
+
+		FastNoise generator = new FastNoise(new Random().nextInt());
+		generator.SetNoiseType(NoiseType.Value);
+		for (int yc = 0; yc < size; yc++) {
+			for (int xc = 0; xc < size; xc++) {
+				double max = 0;
+				// there are more efficient algorithms than this
+				int R = TREE_PROBABILITY_TABLE.get(biome[xc][yc]);
+				for (int yn = yc - R; yn <= yc + R; yn++) {
+					for (int xn = xc - R; xn <= xc + R; xn++) {
+						double e = generator.GetWhiteNoiseInt(xn, yn);
+						if (e > max) {
+							max = e;
+						}
+					}
+				}
+				if (generator.GetWhiteNoiseInt(xc, yc) == max) {
+					// place tree at xc,yc
+					trees.add(new Point.Double(xc, yc));
+				}
+			}
+		}
+		return trees;
 	}
 
 }
